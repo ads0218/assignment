78ff0af5c589ceb41fb9b6269bf6f4c47d9308ce
Seunghyun Back <ads0218@naver.com>
2017-01-23 16:59:46 +0900

    a

--- tabui/tab.js
+++ tabui/tab.js
@@ -1,6 +1,16 @@
-var nav=document.querySelector('nav');
-nav.addEventListener('click',clickEvt);
-var tag;
+document.addEventListener('DOMContentLoaded',function(){
+	init();
+});
+
+function select(sel){
+	return document.querySelector(sel);
+}
+
+function init(){
+	var nav=select('nav');
+	nav.addEventListener('click',clickEvt);
+	var tag;
+}
 
 function clickEvt(evt){
 	tag=evt.target;
@@ -11,25 +21,59 @@ function clickEvt(evt){
 		return;
 	}
 	changeColor(tag);
-	var url=tag.dataset.url;
-	callAjax(url,writeData);
+
+	if(tag.dataset.click==0){
+		var url=tag.dataset.url;
+		callAjax(url,writeData);
+		tag.dataset.click++;
+		return;
+	}
+
+	var id='my_'+tag.id;
+
+	var section=select('#'+id);
+	var delSection=select('.eleDisplayShow')
+
+	delSection.classList.toggle('eleDisplayShow');
+	section.classList.toggle('eleDisplayShow');
+
 }
 
 function writeData(){
 	var data=JSON.parse(this.responseText);
 	var id='my_'+tag.id;
-	var section=document.querySelector('#'+id);
-	var delSection=document.querySelector('.eleDisplayShow')
+
+	var section=select('#'+id);
+	var delSection=select('.eleDisplayShow')
 	var content=delSection.innerHTML;
-	delSection.innerHTML='';
+
+	//use saving at html
+	var newContent = select('#temp').innerHTML
+	newContent=newContent.replace('{{title}}',data.title);
+	newContent=newContent.replace('{{body}}',data.body);
+	section.innerHTML=newContent;
+	//end use saving at html
+
+	// //use string replace
+	// var delMyName = select('.myName').innerHTML;
+	// var delMyDesc = select('.myDesc').innerHTML;
+
+	// var newContent = content.replace(delMyName,data.title);
+	// newContent = newContent.replace(delMyDesc,data.body);
+
+	// section.innerHTML = newContent;
+	// //end use string replace
+
 	delSection.classList.toggle('eleDisplayShow');
 	section.classList.toggle('eleDisplayShow');
-	section.innerHTML=content;
-	var myName=document.querySelector('.myName');
-	var myDesc=document.querySelector('.myDesc');
-	myName.innerHTML=data.title;
-	myDesc.innerHTML=data.body;
-}
+
+	//section.innerHTML=content;
+	// var myName=select('.myName');
+	// var myDesc=select('.myDesc');
+	// myName.innerHTML=data.title;
+	// myDesc.innerHTML=data.body;
+
+	}
 
 function callAjax(url,fn){
 	var XHR=new XMLHttpRequest();
@@ -39,7 +83,7 @@ function callAjax(url,fn){
 }
 
 function changeColor(nav){
-	var selectedTab=document.querySelector('.selectedTab');
+	var selectedTab=select('.selectedTab');
 	selectedTab.classList.toggle('selectedTab');
 	nav.classList.toggle('selectedTab');
 }
